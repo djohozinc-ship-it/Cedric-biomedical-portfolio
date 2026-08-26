4027b2138ff8f29de0f90b3bd70b73574b664e24
cedo <djohozinc@gmail.com>
2026-08-26 19:25:23 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
