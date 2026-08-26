922779ac39d0a1c8f8ee3dd18d628697807f6c51
cedo <djohozinc@gmail.com>
2026-08-26 19:19:29 +0100

    Update footer with new social media links
    Updated footer links to new GitHub and LinkedIn profiles, added email link.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
 
@@ -7,8 +8,32 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a 
+          href="https://github.com/djohozinc-ship-it" 
+          target="_blank" 
+          rel="noreferrer"
+          aria-label="GitHub"
+        >
+          <GitHubIcon />
+        </a>
+
+        <a 
+          href="https://www.linkedin.com/in/concepteurdemaintenance" 
+          target="_blank" 
+          rel="noreferrer"
+          aria-label="LinkedIn"
+        >
+          <LinkedInIcon />
+        </a>
+
+        <a 
+          href="mailto:djohozinc@gmail.com"
+          aria-label="Email"
+        >
+          <EmailIcon />
+        </a>
+
+        <p>Cédric DJOHOZIN djohozinc@gmail.com</p>
       </div>
     </footer>
   );
